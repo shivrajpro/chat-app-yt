1259e2718a05c4a9d9a49b0ef5d3241c55f71e3b
shivrajpro <shivrajhulbatte@gmail.com>
2024-02-16 17:02:32 +0530

    generate jwt

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,4 +1,6 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
   try {
@@ -15,8 +17,8 @@ export const signup = async (req, res) => {
     }
 
     // HASH PASSWORD HERE
-    // https://avatar-placeholder.iran.liara.run/
-    // https://avatar.iran.liara.run/public/boy
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
     const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
@@ -24,19 +26,23 @@ export const signup = async (req, res) => {
     const newUser = new User({
       fullName,
       username,
-      password,
+      password: hashedPassword,
       gender,
       profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
     });
 
-    await newUser.save();
-
-    res.status(201).json({
-      _id: newUser._id,
-      fullName: newUser.fullName,
-      username: newUser.fullName,
-      profilePic: newUser.profilePic,
-    });
+    if(newUser){
+      // generate jwt here
+      generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
+  
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.fullName,
+        profilePic: newUser.profilePic,
+      });
+    }
   } catch (error) {
     console.log("error in signup controller", error.message);
     res.status(500).json({ error: "Internal Server Error" });
