4555caead6e4d5652d43f7cc6eb41ab0f468cd3d
shivrajpro <shivrajhulbatte@gmail.com>
2024-02-19 16:44:43 +0530

    UI - build login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,20 +2,15 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Login from "./pages/login/Login";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
-    </>
+    <div className="p-4 h-screen flex items-center justify-center" >
+      <Login />
+    </div>
   );
 }
 
