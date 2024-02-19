b2edeacadbe8a6dc56aa95030f85144b74b03998
shivrajpro <shivrajhulbatte@gmail.com>
2024-02-19 14:55:57 +0530

    get message endpoint

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -43,3 +43,20 @@ export const sendMessage = async (req, res) => {
     res.status(500).json({ error: "internal server error" });
   }
 };
+export const getMessages = async(req, res)=>{
+  try {
+    const {id: userToChatId} = req.params;
+
+    const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+      participants:{ $all: [senderId, userToChatId]}
+    }).populate('messages');
+
+    if(!conversation) return res.status(200).json([]);
+    res.status(200).json(conversation.messages);
+  } catch (error) {
+    console.log("error in getmessages controller",error.message);
+    res.status(500).json({error:"internal server error"})
+  }
+}
