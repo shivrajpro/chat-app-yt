44cc880cc6d2725d7c8b9dc064c4c369139fe679
shivrajpro <shivrajhulbatte@gmail.com>
2024-02-19 15:14:14 +0530

    get users for sidebar

---
+++ backend/controllers/user.conttroller.js
@@ -0,0 +1,14 @@
+import User from "../models/user.model.js";
+
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+
+    const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password")
+    //not equal to
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.error("error in getusersforSidebar", error.message);
+    res.status(500).json({ error: "internal server error" });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import cookieParser from "cookie-parser";
 
@@ -20,6 +23,7 @@ app.get("/", (req, res) => {
  */
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
     connectToMongoDB();
