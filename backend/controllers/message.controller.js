a699e8abb1dabf1f4c9f050dd2f0db4dc2ce0ff2
shivrajhulbatte@gmail.com <shivrajhulbatte@gmail.com>
2024-02-22 19:48:33 +0530

    create useListenMessages hook

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessage = async (req, res) => {
   // console.log("message sent", req.params.id);
@@ -28,35 +29,39 @@ export const sendMessage = async (req, res) => {
     if (newMessage) {
       conversation.messages.push(newMessage._id); //put the message in array, if this is not 1st message
     }
-
-    // SOCKET IO FUNCTIONANLITY WILL GO HERE
-  
     // collections were empty before these two lines
     // await conversation.save();
     // await newMessage.save();
 
     await Promise.all([conversation.save(), newMessage.save()]); //optimization
 
+    // SOCKET IO FUNCTIONANLITY WILL GO HERE
+    const receiverSocketId = getReceiverSocketId(receiverId);
+
+    if (receiverSocketId) {
+      // io.to(<socket_id>).emit() used to send events to specific client
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
     res.status(201).json(newMessage);
   } catch (error) {
     console.log("error in sendMessage controller", error.message);
     res.status(500).json({ error: "internal server error" });
   }
 };
-export const getMessages = async(req, res)=>{
+export const getMessages = async (req, res) => {
   try {
-    const {id: userToChatId} = req.params;
+    const { id: userToChatId } = req.params;
 
     const senderId = req.user._id;
 
     const conversation = await Conversation.findOne({
-      participants:{ $all: [senderId, userToChatId]}
+      participants: { $all: [senderId, userToChatId] }
     }).populate('messages');
 
-    if(!conversation) return res.status(200).json([]);
+    if (!conversation) return res.status(200).json([]);
     res.status(200).json(conversation.messages);
   } catch (error) {
-    console.log("error in getmessages controller",error.message);
-    res.status(500).json({error:"internal server error"})
+    console.log("error in getmessages controller", error.message);
+    res.status(500).json({ error: "internal server error" })
   }
 }
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -2,9 +2,11 @@ import { useEffect, useRef } from "react";
 import useGetMessages from "../../hooks/useGetMessages";
 import MessageSkeleton from "../skeletions/MessageSkeletion";
 import Message from "./Message";
+import useListenMessages from "../../hooks/useListenMessages";
 
 const Messages = () => {
   const { messages, loading } = useGetMessages();
+  useListenMessages();
   // console.log("messages", messages);
   const lastMessageRef = useRef();
 
