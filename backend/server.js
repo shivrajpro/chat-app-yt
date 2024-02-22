2a948ffea97526c468ad073abf91392501733e03
shivrajhulbatte@gmail.com <shivrajhulbatte@gmail.com>
2024-02-22 18:49:02 +0530

    initiate web socket connection

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,9 @@ import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import cookieParser from "cookie-parser";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
+// const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config(); //to capture the PORT number fron .env file
@@ -25,7 +26,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     connectToMongoDB();
     console.log(`server running on port ${PORT}`)
 });
